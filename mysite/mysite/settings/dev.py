63d304407a9a36af4b3dd2e838feee4d9cba4a74
Nat Weerawan <nat.wrw@gmail.com>
2021-08-21 13:25:49 +0000

    Add mysite

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-&6p5yx*ic^dkq##04s$x5i%0wb!70tr-t^p+y_+kyze49dut(('
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
